f761d606b5afc74cbffad3ef58476c2e94c6ff99
SkyTheNeko <brtnik.roman@gmail.com>
2026-03-11 23:43:24 +0100

    Finalized the basics, made PM into prefab, small hierarchy changs

--- Assets/UI/Scripts/MainMenu.cs
+++ Assets/UI/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartRun()
     {
-        SceneManager.LoadScene("FirstMap");
+        SceneManager.LoadScene("DeepForest-FirstMap");
     }
 
     public void GoToSettingsMenu()
