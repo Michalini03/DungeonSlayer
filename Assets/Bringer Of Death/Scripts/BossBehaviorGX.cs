a8ea72b8a9f83377c3eb5913c67c61c9657d3550
Pavel Kratochvíle <pavel.kratochvile@gmail.com>
2026-05-02 11:13:27 +0200

    Casting enemies and added boss hit logic

--- Assets/Bringer Of Death/Scripts/BossBehaviorGX.cs
+++ Assets/Bringer Of Death/Scripts/BossBehaviorGX.cs
@@ -4,6 +4,7 @@ public class BossBehaviorGX : MonoBehaviour
 {
     [SerializeField]
     private BossBehavior bossBehavior;
+    public Animator animator;
 
     public void CastSpellParent()
     {
